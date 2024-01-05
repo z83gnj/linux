7dcded4b1fcb7f06c63fe2db13b31e7482ee0535
Ákos Matiny <akos.matiny@gmail.com>
2024-01-05 21:58:15 +0100

    up

--- debian_setup.sh
+++ debian_setup.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
 apt-get install gnome-core -y
+apt-get gpg -y
 
 # Replace the default .bashrc 
 mv ~/.bashrc default.bashrc
@@ -11,11 +12,16 @@ TEMP_DIR=$(mktemp -d)
 
 # Install Google Chrome
 wget -qnv --show-progress -O ${TEMP_DIR}/google-chrome-stable_current_amd64.deb  https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb 
-apt-get install ${TEMP_DIR}/google-chrome-stable_current_amd64.deb
+apt-get install ${TEMP_DIR}/google-chrome-stable_current_amd64.deb -y
 
 # Install Visual Studio Code
-wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | apt-key add - 
-add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+#wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | apt-key add - 
+#add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+#
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+rm -f packages.microsoft.gpg
 apt-get update && apt-get install code -y
 
 # Install LibreOffice
