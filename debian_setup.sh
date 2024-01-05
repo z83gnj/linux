24e47f7972a96ef59b667fafcb30b5c2eb157576
Ákos Matiny <akos.matiny@gmail.com>
2024-01-05 17:48:09 +0100

    up

--- debian_setup.sh
+++ debian_setup.sh
@@ -11,17 +11,17 @@ TEMP_DIR=$(mktemp -d)
 
 # Install Google Chrome
 wget -qnv --show-progress -O ${TEMP_DIR}/google-chrome-stable_current_amd64.deb  https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb 
-apt install ${TEMP_DIR}/google-chrome-stable_current_amd64.deb
+apt-get install ${TEMP_DIR}/google-chrome-stable_current_amd64.deb
 
 # Install Visual Studio Code
 wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | apt-key add - 
 add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
-apt update && apt install code -y
+apt-get update && apt-get install code -y
 
 # Install PowerShell
 # sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-debian-bullseye-prod bullseye main" > /etc/apt/sources.list.d/microsoft.list' 
 add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-debian-bullseye-prod bullseye main"
-apt update && apt install powershell -y
+apt-get update && apt-get install powershell -y
 
 
 # Remove tmp folder
