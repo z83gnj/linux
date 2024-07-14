c09ce97bdc637fec8ebe58d6294133923e743f92
Ákos Matiny <akos.matiny@gmail.com>
2024-07-14 21:25:00 +0200

    upds

--- 01_DownServer/dowload-srv.sh
+++ 01_DownServer/dowload-srv.sh
@@ -1,36 +1,38 @@
+############################################
+# User input
+# Please add username and folder to 
+# the transmission settings file and the 
+# smb.conf before running the script
+############################################
+
+PATH_OF_SMB_SHARE=/path/to/folder
+SMB_USER=akos
+
+############################################
+# Start of the script
+############################################
 sudo apt insatll samba -y
 
-sudo useradd -M -N akos # user for smb share
+sudo useradd -M -N "$SMB_USER" # user for smb share
 sudo groupadd smb # create group for smb share
-sudo usermod -g smb akos  #change user primary group
+sudo usermod -g smb "$SMB_USER"  #change user primary group
 # Permission for the smb share
-# sudo chown -R akos:smb /path/to/folder
+sudo chown -R "$SMB_USER":smb "$PATH_OF_SMB_SHARE"
 
-sudo smbpasswd -a akos # set password for smb share
+sudo smbpasswd -a "$SMB_USER" # set password for smb share
 
-sudo mv /etc/samba/smb.conf /etc/samba/smb.conf.bak
-
-sudo cat << EOF >> /etc/samba/smb.conf
-[global]
-    workgroup = WORKGROUP
-    log file = /var/log/samba/log.%m
-    max log size = 10000
-    logging = file
-    server role = standalone server
-    unix password sync = yes
-    passwd program = /usr/bin/passwd %u
-    passwd chat = *Enter\snew\s*\spassword:* %n\n *Retype\snew\s*\spassword:* %n\n *password\supdated\ssuccessfully* .
-
-[TorrentDown]
-    commanet = Torrent downloads
-    path = /mnt/torrent/
-    valid users = akos
-    read only = no
-    create mask = 0777
-    directory mask = 0777
-
-EOF
-
+sudo mv -f /etc/samba/smb.conf /etc/samba/smb.conf.bak
+sudo cp -f ./Settings_files/smb.conf /etc/samba/smb.conf
 testparm
 
 sudo systemctl restart smbd
+
+apt install transmission-daemon -y
+sudo systemctl stop transmission-daemon
+
+sudo usermod -g smb debian-transmission
+sudo usermod -aG debian-transmission debian-transmission
+
+sudo cp -f ./Settings_Files/setting.json /etc/transmission-daemon/setting.json
+
+sudo systemctl start transmission-daemon
